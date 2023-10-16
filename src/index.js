e37ba31f852045d09a2a12ff39561103ef8b3ec5
Philip Fung <boo@boo.boo>
2023-10-16 13:37:39 -0700

    added a way to set maximum number of times the modal is shown using the option maxModalDisplayCount

--- src/index.js
+++ src/index.js
@@ -2,41 +2,45 @@ import './styles.css';
 
 class AddToHomeScreen {
 
-/**
- * Summary. (use period)
- *
- * Description. (use period)
- *
- *
- * @param {string} appName                                 The name of the app e.g. 'Aardvark'.  
- *                                                         Try to keep as short as possible as this is displayed in the install message on the device.
- * 
- * @param {URL} appIconUrl                                 URL link to the app icon e.g. "https://aardvark.app/icon.png" .  
- *                                                         Should have rounded corners, square, and larger than 40 x 40 pixels.
- *
- * 
- * @param (URL) assetUrl                                   
- * @param {boolean} showErrorMessageForUnsupportedBrowsers If true, show an error message if the user is on an unsupported browser/device on IOS or Android
- *                                                         e.g 
- *                                                         "On IOS, adding to homescreen is only supported on Safari.  
- *                                                         Please open this website on Safari [Copy link to clipboard]"
- *                                                         Default is true.
- * @param {boolean} allowUserToCloseModal                  If true, user can close the modal if they click outside the modal window.  
- *                                                         (I've found that user is way more likely to install if user has no other option.) 
- *                                                         Default is false.
- */
+  /**
+   * Summary. (use period)
+   *
+   * Description. (use period)
+   *
+   *
+   * @param {string} appName                                 The name of the app e.g. 'Aardvark'.  
+   *                                                         Try to keep as short as possible as this is displayed in the install message on the device.
+   * 
+   * @param {URL} appIconUrl                                 URL link to the app icon e.g. "https://aardvark.app/icon.png" .  
+   *                                                         Should have rounded corners, square, and larger than 40 x 40 pixels.
+   *
+   * 
+   * @param (URL) assetUrl                                   
+   * @param {boolean} showErrorMessageForUnsupportedBrowsers If true, show an error message if the user is on an unsupported browser/device on IOS or Android
+   *                                                         e.g 
+   *                                                         "On IOS, adding to homescreen is only supported on Safari.  
+   *                                                         Please open this website on Safari [Copy link to clipboard]"
+   *                                                         Default is true.
+   * @param {boolean} allowUserToCloseModal                  If true, user can close the modal if they click outside the modal window.  
+   *                                                         (I've found that user is way more likely to install if user has no other option.) 
+   *                                                         Default is false.
+   * @param {int} maxModalDisplayCount                       If set, the modal will only show this many times.
+   *                                                         Default is -1 (no limit).  (Debugging: Use this.clearModalDisplayCount() to reset the count)
+   */
   constructor({
     appName,
     appIconUrl,
     assetUrl,
     showErrorMessageForUnsupportedBrowsers,
-    allowUserToCloseModal
+    allowUserToCloseModal,
+    maxModalDisplayCount
   }) {
     this.appName = appName;
     this.appIconUrl = appIconUrl;
     this.assetUrl = assetUrl;
     this.showErrorMessageForUnsupportedBrowsers = (typeof showErrorMessageForUnsupportedBrowsers === "undefined") ? true : showErrorMessageForUnsupportedBrowsers;
     this.allowUserToCloseModal = (typeof allowUserToCloseModal === "undefined") ? false : allowUserToCloseModal;
+    this.maxModalDisplayCount = (typeof maxModalDisplayCount === "undefined") ? -1 : maxModalDisplayCount;
 
     this.closeEventListener = null;
   }
@@ -55,19 +59,19 @@ class AddToHomeScreen {
     return navigator.userAgent.match(/iPhone|iPad|iPod/);
   }
 
-    /* Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)
-     AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0
-     Mobile/14E5239e Safari/602.1 */
+  /* Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)
+   AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0
+   Mobile/14E5239e Safari/602.1 */
   isBrowserIOSSafari() {
-    return this.isDeviceIOS() && 
-    window.navigator.userAgent.match(/Safari/) && 
-    !this.isBrowserIOSChrome() &&
-    !this.isBrowserIOSFirefox() &&
-    !this.isBrowserIOSInAppFacebook() &&
-    !this.isBrowserIOSInAppLinkedin() &&
-    !this.isBrowserIOSInAppInstagram() &&
-    !this.isBrowserIOSInAppThreads() &&
-    !this.isBrowserIOSInAppTwitter();
+    return this.isDeviceIOS() &&
+      window.navigator.userAgent.match(/Safari/) &&
+      !this.isBrowserIOSChrome() &&
+      !this.isBrowserIOSFirefox() &&
+      !this.isBrowserIOSInAppFacebook() &&
+      !this.isBrowserIOSInAppLinkedin() &&
+      !this.isBrowserIOSInAppInstagram() &&
+      !this.isBrowserIOSInAppThreads() &&
+      !this.isBrowserIOSInAppTwitter();
   }
 
   /* Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)
@@ -105,8 +109,8 @@ class AddToHomeScreen {
     }
 
 
-        // TODO: this is incompatabile with Insta/Threads mobile website links.
-        // TODO: this solution only works with first-level links
+    // TODO: this is incompatabile with Insta/Threads mobile website links.
+    // TODO: this solution only works with first-level links
     if (window.document.referrer.match('//l.instagram.com/')) {
       return true;
     }
@@ -139,11 +143,11 @@ class AddToHomeScreen {
   /* Mozilla/5.0 (Linux; Android 10) 
      AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.92 Mobile Safari/537.36 */
   isBrowserAndroidChrome() {
-    return this.isDeviceAndroid() && 
-    window.navigator.userAgent.match(/Chrome/) &&
-    !this.isBrowserAndroidFacebook() &&
-    !this.isBrowserAndroidSamsung() &&
-    !this.isBrowserAndroidFirefox();
+    return this.isDeviceAndroid() &&
+      window.navigator.userAgent.match(/Chrome/) &&
+      !this.isBrowserAndroidFacebook() &&
+      !this.isBrowserAndroidSamsung() &&
+      !this.isBrowserAndroidFirefox();
   }
 
   /*Mozilla/5.0 (Linux; Android 12; SM-S908U1 Build/SP1A.210812.016; wv) 
@@ -165,22 +169,10 @@ class AddToHomeScreen {
   }
 
   show() {
-    // the main function 
-    // show the message modal to the user to add app to home screen
-    // returns an object indicating what device/browser was detected, in case user wants
-    // to use that info for some other UI purpose
-    const container = document.createElement('div');
-    container.classList.add('adhs-container');
-
-    // dark overlay covers entire body
-    container.style.height = document.body.clientHeight + 'px';
-    //container.style.width = document.body.clientWidth + 'px';
-    container.style.width = window.innerWidth + 'px';
 
     var ret;
-    var _isStandAlone = this.isStandAlone();
 
-    if (_isStandAlone) { // already installed
+    if (this.isStandAlone()) {
       ret = new AddToHomeScreen.ReturnObj(
         {
           isStandAlone: true,
@@ -188,122 +180,140 @@ class AddToHomeScreen {
           device: (this.isDeviceIOS() ? 'IOS' : 'ANDROID')
         }
       );
-    } else if (this.isDeviceIOS()) { // ios
-      if (this.isBrowserIOSSafari()) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: true,
-            device: 'IOS'
-          }
-        );
-        this._genIOSSafari(container);
-      } else if (this.isBrowserIOSChrome()) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: true,
-            device: 'IOS'
-          }
-        );
-        this._genIOSChrome(container);
-      } else if (
-        this.isBrowserIOSInAppFacebook() ||
-        this.isBrowserIOSInAppLinkedin()
-        ) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: false,
-            device: 'IOS'
-          }
-        );
-        this._genIOSInAppBrowserOpenInSystemBrowser(container);
-      } else if (
-        this.isBrowserIOSInAppInstagram() ||
-        this.isBrowserIOSInAppThreads() ||
-        this.isBrowserIOSInAppTwitter()
-      ) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: false,
-            device: 'IOS'
-          }
-        );
-        this._genIOSInAppBrowserOpenInSafariBrowser(container);
-      } else  {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: false,
-            device: 'IOS'
-          }
-        );
-        if (this.showErrorMessageForUnsupportedBrowsers) {
-          this._genErrorMessage(
-            container,
-            `Please open this website with the Safari or Chrome app.`,
-            `Adding to home screen is only supported in Safari or Chrome on IOS.`
-          );
-        }
-      }
-    } else if (this.isDeviceAndroid()) { // android
-      if (this.isBrowserAndroidChrome()) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: true,
-            device: 'ANDROID'
-          }
-        );
-        this._genAndroidChrome(container);
-      } else if (this.isBrowserAndroidFacebook()) {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: false,
-            device: 'ANDROID'
-          }
-        );
-        this._genIOSInAppBrowserOpenInSystemBrowser(container);
-      } else {
-        ret = new AddToHomeScreen.ReturnObj(
-          {
-            isStandAlone: false,
-            canBeStandAlone: false,
-            device: 'ANDROID'
-          }
-        );
-        if (this.showErrorMessageForUnsupportedBrowsers) {
-          this._genErrorMessage(
-            container,
-            `Please open this website with the Chrome app.`,
-            `Adding to home screen is only supported in Chrome on Android.`
-          );
-        }
-      }
-    } else { // desktop
+      return ret;
+    } else if (this._hasReachedMaxModalDisplayCount()) {
       ret = new AddToHomeScreen.ReturnObj(
         {
-          isStandAlone: false,
-          canBeStandAlone: false,
-          device: ''
+          isStandAlone: null,
+          canBeStandAlone: null,
+          device: (this.isDeviceIOS() ? 'IOS' : 'ANDROID')
         }
       );
-      if (this.showErrorMessageForUnsupportedBrowsers) {
-        this._genErrorMessage(
-          container,
-          `Please open this website on a mobile device.`,
-          `Installing to your home screen is currently only supported on IOS and Android.`
-        );
-      }
-    }
+      return ret;
+    } else {
+      this._incrModalDisplayCount();
+      const container = document.createElement('div');
+      container.classList.add('adhs-container');
 
-    if (!_isStandAlone) {
+      // dark overlay covers entire body
+      container.style.height = document.body.clientHeight + 'px';
+      //container.style.width = document.body.clientWidth + 'px';
+      container.style.width = window.innerWidth + 'px';
+
+      if (this.isDeviceIOS()) { // ios
+        if (this.isBrowserIOSSafari()) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: true,
+              device: 'IOS'
+            }
+          );
+          this._genIOSSafari(container);
+        } else if (this.isBrowserIOSChrome()) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: true,
+              device: 'IOS'
+            }
+          );
+          this._genIOSChrome(container);
+        } else if (
+          this.isBrowserIOSInAppFacebook() ||
+          this.isBrowserIOSInAppLinkedin()
+        ) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: false,
+              device: 'IOS'
+            }
+          );
+          this._genIOSInAppBrowserOpenInSystemBrowser(container);
+        } else if (
+          this.isBrowserIOSInAppInstagram() ||
+          this.isBrowserIOSInAppThreads() ||
+          this.isBrowserIOSInAppTwitter()
+        ) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: false,
+              device: 'IOS'
+            }
+          );
+          this._genIOSInAppBrowserOpenInSafariBrowser(container);
+        } else {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: false,
+              device: 'IOS'
+            }
+          );
+          if (this.showErrorMessageForUnsupportedBrowsers) {
+            this._genErrorMessage(
+              container,
+              `Please open this website with the Safari or Chrome app.`,
+              `Adding to home screen is only supported in Safari or Chrome on IOS.`
+            );
+          }
+        }
+      } else if (this.isDeviceAndroid()) { // android
+        if (this.isBrowserAndroidChrome()) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: true,
+              device: 'ANDROID'
+            }
+          );
+          this._genAndroidChrome(container);
+        } else if (this.isBrowserAndroidFacebook()) {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: false,
+              device: 'ANDROID'
+            }
+          );
+          this._genIOSInAppBrowserOpenInSystemBrowser(container);
+        } else {
+          ret = new AddToHomeScreen.ReturnObj(
+            {
+              isStandAlone: false,
+              canBeStandAlone: false,
+              device: 'ANDROID'
+            }
+          );
+          if (this.showErrorMessageForUnsupportedBrowsers) {
+            this._genErrorMessage(
+              container,
+              `Please open this website with the Chrome app.`,
+              `Adding to home screen is only supported in Chrome on Android.`
+            );
+          }
+        }
+      } else { // desktop
+        ret = new AddToHomeScreen.ReturnObj(
+          {
+            isStandAlone: false,
+            canBeStandAlone: false,
+            device: ''
+          }
+        );
+        if (this.showErrorMessageForUnsupportedBrowsers) {
+          this._genErrorMessage(
+            container,
+            `Please open this website on a mobile device.`,
+            `Installing to your home screen is currently only supported on IOS and Android.`
+          );
+        }
+      }
+
       document.body.appendChild(container);
       this._registerCloseListener();
-
       setTimeout(() => {
         container.classList.add('visible');
       }, 50);
@@ -329,6 +339,7 @@ class AddToHomeScreen {
   }
 
   // below are all internal functions
+
   _genLogo() {
     return `
       <div class="adhs-logo">
@@ -392,7 +403,7 @@ class AddToHomeScreen {
   }
 
   _genIOSSafari(container) {
-    var containerInnerHTML = 
+    var containerInnerHTML =
       this._genLogo() +
       this._genModalStart() +
       this._genTitle() +
@@ -411,7 +422,7 @@ class AddToHomeScreen {
   }
 
   _genIOSChrome(container) {
-    var containerInnerHTML = 
+    var containerInnerHTML =
       this._genLogo() +
       this._genModalStart() +
       this._genTitle() +
@@ -430,7 +441,7 @@ class AddToHomeScreen {
   }
 
   _genIOSInAppBrowserOpenInSystemBrowser(container) {
-    var containerInnerHTML = 
+    var containerInnerHTML =
       this._genLogo() +
       this._genModalStart() +
       this._genTitle() +
@@ -448,7 +459,7 @@ class AddToHomeScreen {
   }
 
   _genIOSInAppBrowserOpenInSafariBrowser(container) {
-    var containerInnerHTML = 
+    var containerInnerHTML =
       this._genLogo() +
       this._genModalStart() +
       this._genTitle() +
@@ -465,14 +476,14 @@ class AddToHomeScreen {
   }
 
   _genAndroidChrome(container) {
-    var containerInnerHTML = 
+    var containerInnerHTML =
       this._genLogo() +
       this._genModalStart() +
       this._genTitle() +
       this._genListStart() +
       this._genListItem(`1`, `Tap the <img class="adhs-android-chrome-more-button" src="` + this._genAssetUrl('android-chrome-more-button.svg') + `"/> button in the browser bar.`) +
-      this._genListItem(`2`, `Tap <img class="adhs-android-chrome-add-to-homescreen-button" src="` + this._genAssetUrl('android-chrome-add-to-home-screen-button.svg') + `"/>` +  
-      `or <img class="adhs-android-chrome-install-app" src="` + this._genAssetUrl('android-chrome-install-app.svg') + `"/> .`) +
+      this._genListItem(`2`, `Tap <img class="adhs-android-chrome-add-to-homescreen-button" src="` + this._genAssetUrl('android-chrome-add-to-home-screen-button.svg') + `"/>` +
+        `or <img class="adhs-android-chrome-install-app" src="` + this._genAssetUrl('android-chrome-install-app.svg') + `"/> .`) +
       this._genListItem(`3`, `Open the <img class="adhs-your-app-icon" src="` + this.appIconUrl + `"/> app.`) +
       this._genListEnd() +
       this._genModalEnd() +
@@ -496,6 +507,45 @@ class AddToHomeScreen {
       };
       window.addEventListener('touchstart', this.closeEventListener);
     }
+  }
+
+  clearModalDisplayCount() {
+    if (this._isEnabledModalDisplayCount()) {
+      window.localStorage.removeItem('adhs-modal-display-count');
+    }
+  }
+
+  _isEnabledModalDisplayCount() {
+    return (typeof (this.maxModalDisplayCount) === 'number') && (this.maxModalDisplayCount >= 0) && window.localStorage;
+  }
+
+  _hasReachedMaxModalDisplayCount() {
+    if (!this._isEnabledModalDisplayCount()) {
+      return false;
+    }
+    return (this._getModalDisplayCount() >= this.maxModalDisplayCount);
+  }
+
+  _incrModalDisplayCount() {
+    if (!this._isEnabledModalDisplayCount()) {
+      return false;
+    }
+
+    var count = this._getModalDisplayCount();
+    count++;
+    window.localStorage.setItem('adhs-modal-display-count', count);
+    return true;
+  }
+
+  _getModalDisplayCount() {
+    var count = window.localStorage.getItem('adhs-modal-display-count');
+    if (count === null) {
+      count = 0;
+      window.localStorage.setItem('adhs-modal-display-count', count);
+    } else {
+      count = parseInt(count);
+    }
+    return count;
   }
 
   static copyToClipboard() {
