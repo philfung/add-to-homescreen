f2884a628316f3598d4a0c7ab7b5d126633397ce
Philip Fung <no@one>
2024-09-02 19:10:17 -0700

    ios chrome

--- src/index.ts
+++ src/index.ts
@@ -532,9 +532,7 @@ class AddToHomeScreen {
       this._genAppNameHeader() +
       this._genAppUrlHeader() +
       this._genListStart() +
-      // this._genListItem(`1`, i18n.__('Tap the %s button bellow.', `<img class="adhs-ios-safari-sharing-api-button" src="${this._genAssetUrl('ios-safari-sharing-api-button.svg')}" />`)) +
       this._genListItem(`1`, i18n.__('Tap the %s button in the toolbar.', this._genListButtonWithImage(this._genAssetUrl('ios-safari-sharing-api-button-2.svg')))) +
-      // this._genListItem(`2`, i18n.__('Select %s from the menu that pops up.', `<img class="adhs-ios-safari-add-to-home-screen-button" src="${this._genAssetUrl('ios-safari-add-to-home-screen-button.svg')}" />`) + ` <span class="adhs-emphasis">${i18n.__('You may need to scroll down to find this menu item.')}</span>`) +
       this._genListItem(`2`, i18n.__('Select %s from the menu that pops up.', this._genListButtonWithImage(this._genAssetUrl('ios-safari-add-to-home-screen-button-2.svg'), i18n.__('Add to Home Screen'), 'right')) + ` <span class="adhs-emphasis">${i18n.__('You may need to scroll down to find this menu item.')}</span>`) +
       this._genListItem(`3`, i18n.__('Open the %s app.', `<img class="adhs-your-app-icon" src="${this.appIconUrl}"/>`)) +
       this._genListEnd() +
@@ -544,7 +542,6 @@ class AddToHomeScreen {
     </div>`;
     container.innerHTML = containerInnerHTML;
     container.classList.add('adhs-mobile');
-    // TODO: add adhs-mobile and fix every mobile class
     container.classList.add('adhs-ios');
     container.classList.add('adhs-safari');
   }
@@ -558,8 +555,12 @@ class AddToHomeScreen {
       this._genAppNameHeader() +
       this._genAppUrlHeader() +
       this._genListStart() +
-      this._genListItem(`1`, i18n.__('Tap the %s button in the upper right corner.', `<img class="adhs-ios-chrome-more-button" src="${this._genAssetUrl('ios-chrome-more-button.svg')}"/>`)) +
-      this._genListItem(`2`, i18n.__('Select %s from the menu that pops up.', `<img class="adhs-ios-chrome-add-to-home-screen-button" src="${this._genAssetUrl('ios-chrome-add-to-home-screen-button.svg')}"/>`) + ` <span class="adhs-emphasis">${i18n.__('You may need to scroll down to find this menu item.')}</span>`) +
+      // this._genListItem(`1`, i18n.__('Tap the %s button in the upper right corner.', `<img class="adhs-ios-chrome-more-button" src="${this._genAssetUrl('ios-chrome-more-button.svg')}"/>`)) +
+      this._genListItem(`1`, i18n.__('Tap the %s button in the upper right corner.', this._genListButtonWithImage(this._genAssetUrl('ios-chrome-more-button-2.svg')))) +
+      this._genListItem(`2`, i18n.__('Select %s from the menu that pops up.', 
+        this._genListButtonWithImage(this._genAssetUrl('ios-safari-add-to-home-screen-button-2.svg'), i18n.__('Add to Home Screen'), 'right')) + ` ` +
+        `<span class="adhs-emphasis">${i18n.__('You may need to scroll down to find this menu item.')}</span>`
+      ) +
       this._genListItem(`3`, i18n.__('Open the %s app.', `<img class="adhs-your-app-icon" src="${this.appIconUrl}"/>`)) +
       this._genListEnd() +
       this._genModalEnd() +
